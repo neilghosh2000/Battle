d8144e92006dde187d3ff08b0f3230cb268fe478
neilghosh2000 <39198528+neilghosh2000@users.noreply.github.com>
2019-05-13 01:40:25 +0530

    Added AI for Enemy Attacks

--- main.py
+++ main.py
@@ -23,15 +23,16 @@ grenade = Items("Grenade", "Damage", "Deals 150 damage", 150)
 trap = Items("Trap", "Damage", "Deals 90 damage", 90)
 
 magic = [fire, thunder, blizzard, meteor, earthquake, cure, cura]
+enemy_magic = [fire, thunder, meteor, cura]
 items = [{"name": small_potion, "quantity": 3}, {"name": large_potion, "quantity": 2}, {"name": elixir, "quantity": 1}
          , {"name": grenade, "quantity": 2}, {"name": trap, "quantity": 1}]
 
-# Defining Player and Enemy
+# Defining Players and Enemies
 player1 = Person("Player 1", 1000, 450, 10, 60, magic, items)
 player2 = Person("Player 2", 1000, 450, 10, 60, magic, items)
 player3 = Person("Player 3", 1000, 450, 10, 60, magic, items)
-enemy1 = Person("Enemy 1", 800, 500, 90, 70, [], [])
-enemy2 = Person("Enemy 2", 800, 500, 90, 70, [], [])
+enemy1 = Person("Enemy 1", 800, 500, 90, 70, enemy_magic, [])
+enemy2 = Person("Enemy 2", 800, 500, 90, 70, enemy_magic, [])
 
 players = [player1, player2, player3]
 enemies = [enemy1, enemy2]
@@ -46,12 +47,14 @@ while running:
              "    " + BColors.BOLD + BColors.UNDERLINE + "Health Points" + BColors.END + "                              "
              "      " + BColors.BOLD + BColors.UNDERLINE + "Magic Points" + BColors.END)
 
+    # Show stats for all remaining players and enemies
     for player in players:
         player.show_stats()
     print()
     for enemy in enemies:
         enemy.show_enemy_stats()
 
+    # Player attack phase
     i = 0
     while i < len(players):
         player = players[i]
@@ -145,26 +148,21 @@ while running:
 
         i += 1
 
-    print()
-    for enemy in enemies:
-        enemy_damage = enemy.get_damage()
-        enemy_target = random.randrange(0, len(players))
-        players[enemy_target].hp -= enemy_damage
-        print(BColors.RED + enemy.name + " attacked " + players[enemy_target].name + " for " +
-              str(enemy_damage) + " points." + BColors.END )
-
+    # Checking if player is alive
     for player in players:
         if player.get_hp() <= 0:
             player.hp = 0
             print(BColors.RED + BColors.BOLD + player.name + " has died!" + BColors.END)
             players.remove(player)
 
+    # Checking if enemy is alive
     for enemy in enemies:
         if enemy.get_hp() <= 0:
             enemy.hp = 0
             print(BColors.GREEN + BColors.BOLD + enemy.name + " has been defeated!" + BColors.END)
             enemies.remove(enemy)
 
+    # Checking number of remaining players and enemies
     players_remaining = len(players)
     enemies_remaining = len(enemies)
 
@@ -172,3 +170,49 @@ while running:
         print(BColors.BOLD + BColors.RED + "The Enemies have defeated you!" + BColors.END)
     elif enemies_remaining == 0:
         print(BColors.BOLD + BColors.GREEN + "You have won!" + BColors.END)
+
+    # Enemy attack phase
+    print()
+    for enemy in enemies:
+        enemy_choice = random.randrange(1, 3)
+
+        # If enemy chooses magic
+        if enemy_choice == 2:
+            magic_choice = random.randrange(0, len(enemy.magic))
+            magic_cost = magic[magic_choice].cost
+            magic_damage = magic[magic_choice].damage
+
+            if enemy.get_mp() >= magic_cost:
+                per = enemy.get_hp() / enemy.get_max_hp() * 100
+
+                # Enemy only uses white magic if his health is less than 50%
+                if magic[magic_choice].category == "White" and per <= 50:
+                    enemy.hp += magic_damage
+                    enemy.reduce_mp(magic_choice)
+
+                    if enemy.get_hp() >= enemy.get_max_hp():
+                        magic_damage -= (enemy.get_hp() - enemy.get_max_hp())
+                        enemy.hp = enemy.get_max_hp()
+                    print(BColors.RED + enemy.name + " healed himself using " + magic[magic_choice].name + ", using " +
+                          str(magic_cost) + " magic points for " + str(magic_damage) +
+                          " health." + BColors.END)
+
+                # Enemy uses black magic otherwise
+                else:
+                    enemy.reduce_mp(magic_choice)
+                    enemy_target = random.randrange(0, len(players))
+                    players[enemy_target].hp -= magic_damage
+                    print(BColors.RED + enemy.name + " attacked " + players[enemy_target].name + " using " +
+                          magic[magic_choice].name + ", using " + str(magic_cost) + " magic points and dealing " +
+                          str(magic_damage) + " damage." + BColors.END)
+            else:
+                enemy_choice = 1
+
+        # If enemy chooses to attack
+        if enemy_choice == 1:
+            enemy_damage = enemy.get_damage()
+            enemy_target = random.randrange(0, len(players))
+            players[enemy_target].hp -= enemy_damage
+            print(BColors.RED + enemy.name + " attacked " + players[enemy_target].name + " for",
+                  enemy_damage, "points." + BColors.END)
+
